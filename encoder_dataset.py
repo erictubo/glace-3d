b69025ff066cdd3c9e71ab18690323dafa475989
Eric Tüschenbönner <eric.tueschenboenner@gmail.com>
2024-10-07 20:49:40 +0200

    Tested background mask from depth data

--- encoder_dataset.py
+++ encoder_dataset.py
@@ -117,11 +117,15 @@ class RealFakeDataset(Dataset):
         fake_depth = np.load(self.depth_files[idx])
         diff_depth = np.load(self.depth_files[negative_idx])
 
-        fake_mask = torch.from_numpy(fake_depth > 0).float().unsqueeze(0)
-        diff_mask = torch.from_numpy(diff_depth > 0).float().unsqueeze(0)
+        fake_mask = (fake_depth > 0).astype(np.float32)
+        diff_mask = (diff_depth > 0).astype(np.float32)
 
-        assert fake_mask.shape == fake_image.shape[:2], f"Mask shape {fake_mask.shape} does not match image shape {fake_image.shape[:2]}"
-        assert diff_mask.shape == diff_image.shape[:2], f"Mask shape {diff_mask.shape} does not match image shape {diff_image.shape[:2]}"
+        fake_mask = np.expand_dims(fake_mask, axis=2)
+        diff_mask = np.expand_dims(diff_mask, axis=2)
+
+
+        assert fake_mask.shape[:2] == fake_image.shape[:2], f"Mask shape {fake_mask.shape} does not match image shape {fake_image.shape}"
+        assert diff_mask.shape[:2] == diff_image.shape[:2], f"Mask shape {diff_mask.shape} does not match image shape {diff_image.shape}"
 
         return real_image, fake_image, diff_image, fake_mask, diff_mask, distance
 
@@ -185,7 +189,8 @@ class RealFakeDataset(Dataset):
     
     @staticmethod
     def _rotate_image(image, angle, order, mode='constant'):
-        image = image.permute(1, 2, 0).numpy()
+        image = image.permute(1, 2, 0)
+        image = image.numpy()
         image = rotate(image, angle, order=order, mode=mode)
         image = torch.from_numpy(image).permute(2, 0, 1).float()
         return image
@@ -203,7 +208,10 @@ class RealFakeDataset(Dataset):
         fake_mask = self._resize_image(fake_mask, image_height)
         diff_mask = self._resize_image(diff_mask, image_height)
 
-        real_image, fake_image, diff_image, fake_mask, diff_mask = self._crop_to_smallest([real_image, fake_image, diff_image, fake_mask, diff_mask])
+        real_image, fake_image, diff_image = self._crop_to_smallest([real_image, fake_image, diff_image])
+
+        fake_mask = TF.center_crop(fake_mask, (real_image.size[1], real_image.size[0]))
+        diff_mask = TF.center_crop(diff_mask, (real_image.size[1], real_image.size[0]))
 
         # image_mask = torch.ones((1, real_image.size[1], real_image.size[0]))
 
@@ -212,6 +220,9 @@ class RealFakeDataset(Dataset):
         real_image = self.image_transform_normalized(real_image)
         fake_image = self.image_transform(fake_image)
         diff_image = self.image_transform(diff_image)
+
+        fake_mask = torch.from_numpy(np.array(fake_mask)).float().unsqueeze(0)
+        diff_mask = torch.from_numpy(np.array(diff_mask)).float().unsqueeze(0)
 
         if self.augment:
             if angle is None:
@@ -228,8 +239,8 @@ class RealFakeDataset(Dataset):
             fake_image = fake_image.half()
             diff_image = diff_image.half()
         
-        fake_mask = fake_mask > 0
-        diff_mask = diff_mask > 0
+        fake_mask = fake_mask > 0.5
+        diff_mask = diff_mask > 0.5
         
         assert real_image.shape == fake_image.shape == diff_image.shape == fake_mask.shape == diff_mask.shape, \
             f"Shape mismatch: real {real_image.shape}, fake {fake_image.shape}, diff {diff_image.shape}, fake mask {fake_mask.shape}, diff mask {diff_mask.shape}"
@@ -254,7 +265,7 @@ class RealFakeDataset(Dataset):
 
 
 def custom_collate(batch):
-    masks, real_images, fake_images, diff_images, distances = zip(*batch)
+    real_images, fake_images, diff_images, fake_masks, diff_masks, distances = zip(*batch)
 
     # Find max dimensions
     max_height = max([img.shape[1] for img in real_images])
@@ -278,27 +289,29 @@ def custom_collate(batch):
         b_pad = v_padding - t_pad
 
         # Apply the same padding to all corresponding images and mask
-        real_padded = pad_tensor(real_images[i], l_pad, r_pad, t_pad, b_pad)
-        fake_padded = pad_tensor(fake_images[i], l_pad, r_pad, t_pad, b_pad)
-        diff_padded = pad_tensor(diff_images[i], l_pad, r_pad, t_pad, b_pad)
-        mask_padded = pad_tensor(masks[i].float(), l_pad, r_pad, t_pad, b_pad).bool()
+        real_image_padded = pad_tensor(real_images[i], l_pad, r_pad, t_pad, b_pad)
+        fake_image_padded = pad_tensor(fake_images[i], l_pad, r_pad, t_pad, b_pad)
+        diff_image_padded = pad_tensor(diff_images[i], l_pad, r_pad, t_pad, b_pad)
+        fake_mask_padded = pad_tensor(fake_masks[i].float(), l_pad, r_pad, t_pad, b_pad).bool()
+        diff_mask_padded = pad_tensor(diff_masks[i].float(), l_pad, r_pad, t_pad, b_pad).bool()
 
-        padded_images.append((mask_padded, real_padded, fake_padded, diff_padded))
+        padded_images.append((real_image_padded, fake_image_padded, diff_image_padded, fake_mask_padded, diff_mask_padded))
 
     # Unzip the padded images
-    masks_padded, real_images_padded, fake_images_padded, diff_images_padded = zip(*padded_images)
+    real_images_padded, fake_images_padded, diff_images_padded, fake_masks_padded, diff_masks_padded = zip(*padded_images)
 
     # Stack the padded images
-    masks_padded = torch.stack(masks_padded)
     real_images_padded = torch.stack(real_images_padded)
     fake_images_padded = torch.stack(fake_images_padded)
     diff_images_padded = torch.stack(diff_images_padded)
+    fake_masks_padded = torch.stack(fake_masks_padded)
+    diff_masks_padded = torch.stack(diff_masks_padded)
 
-    assert real_images_padded.shape == fake_images_padded.shape == diff_images_padded.shape == masks_padded.shape, \
+    assert real_images_padded.shape == fake_images_padded.shape == diff_images_padded.shape == fake_masks_padded.shape == diff_masks_padded.shape, \
         f"Shape mismatch: real {real_images_padded.shape}, fake {fake_images_padded.shape}, " \
-        f"diff {diff_images_padded.shape}, mask {masks_padded.shape}"
+        f"diff {diff_images_padded.shape}, fake mask {fake_masks_padded.shape}, diff mask {diff_masks_padded.shape}"
 
-    return masks_padded, real_images_padded, fake_images_padded, diff_images_padded, distances
+    return real_images_padded, fake_images_padded, diff_images_padded, fake_masks_padded, diff_masks_padded, distances
 
 
 if __name__ == '__main__':
@@ -315,29 +328,31 @@ if __name__ == '__main__':
         idx = random.sample(range(len(dataset)), 4)
         batch = dataset[idx]
 
-        # fig, ax = plt.subplots(4, 4)
+        fig, ax = plt.subplots(4, 5)
 
-        # for i, sample in enumerate(batch):
-        #     mask, real, fake, diff, distance = sample
+        for i, sample in enumerate(batch):
+            real, fake, diff, fake_mask, diff_mask, distance = sample
 
-        #     ax[i, 0].imshow(real[0], cmap='gray')
-        #     ax[i, 1].imshow(fake[0], cmap='gray')
-        #     ax[i, 2].imshow(diff[0], cmap='gray')
-        #     ax[i, 3].imshow(mask[0], cmap='gray')
+            ax[i, 0].imshow(real[0], cmap='gray')
+            ax[i, 1].imshow(fake[0], cmap='gray')
+            ax[i, 2].imshow(diff[0], cmap='gray')
+            ax[i, 3].imshow(fake_mask[0], cmap='gray')
+            ax[i, 4].imshow(diff_mask[0], cmap='gray')
         
-        # plt.show()
+        plt.show()
 
 
         batch = custom_collate(batch)
 
-        masks, real_images, fake_images, diff_images, distances = batch
+        real_images, fake_images, diff_images, fake_masks, diff_masks, distances = batch
 
-        fig, ax = plt.subplots(4, 4)
+        fig, ax = plt.subplots(4, 5)
 
         for i in range(4):
             ax[i, 0].imshow(real_images[i][0], cmap='gray')
             ax[i, 1].imshow(fake_images[i][0], cmap='gray')
             ax[i, 2].imshow(diff_images[i][0], cmap='gray')
-            ax[i, 3].imshow(masks[i][0], cmap='gray')
+            ax[i, 3].imshow(fake_masks[i][0], cmap='gray')
+            ax[i, 4].imshow(diff_masks[i][0], cmap='gray')
 
         plt.show()
